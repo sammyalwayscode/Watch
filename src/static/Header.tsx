d778be5d6fc72d0107ca55507126f93b763d3c22
sammyalwayscode <olorundasamuel2@gmail.com>
2024-01-06 01:40:47 +0100

    HeroHeader

--- src/static/Header.tsx
+++ src/static/Header.tsx
@@ -6,7 +6,7 @@ const Header = () => {
     <div>
       <div>
         <div>
-          <div>Logo</div>
+          <div>Watch.</div>
           <div>
             <span>Home</span>
             <span>Shop</span>
