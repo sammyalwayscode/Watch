288abba4f3da3a33e40823ee46bf1903267af5ef
sammyalwayscode <olorundasamuel2@gmail.com>
2024-02-13 23:48:57 +0100

    Shop Complected

--- src/pages/ShopDetails.tsx
+++ src/pages/ShopDetails.tsx
@@ -6,8 +6,10 @@ const ShopDetails = () => {
     <div className=" w-full flex justify-center items-center mb-32">
       <div className=" w-[90%] flex flex-col items-center ">
         <section className=" flex flex-col items-center my-16">
-          <h1 className=" font-extrabold text-4xl">Classic Brown Medal</h1>
-          <div className=" flex items-center text-xs font-medium text-gray-400 mt-5">
+          <h1 className=" font-extrabold text-4xl text-center phone:text-2xl">
+            Classic Brown Medal
+          </h1>
+          <div className=" flex items-center text-xs phone:text-[10px] font-medium text-gray-400 mt-5">
             Home{" "}
             <span className=" mx-1">
               <FaAngleRight />
